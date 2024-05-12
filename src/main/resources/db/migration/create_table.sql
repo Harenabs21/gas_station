29b56c039346bada7fe5031f6309a6aefd51dbca
harenabs <hei.harena.6@gmail.com>
2024-05-12 14:40:17 +0300

    feat: database migration file

--- src/main/resources/db/migration/create_table.sql
+++ src/main/resources/db/migration/create_table.sql
@@ -7,36 +7,24 @@ CREATE TABLE IF NOT EXISTS stations (
 CREATE TABLE IF NOT EXISTS products(
     id serial PRIMARY KEY,
     product_name VARCHAR(100) not null,
-    product_capacity double not null,
-    unit_price DOUBLE not null,
-    evaporation_rate int not null
-);
+    unit_price DOUBLE PRECISION not null
+); 
 
-CREATE TABLE IF NOT EXISTS product_sales(
-    id serial PRIMARY KEY,
-    sales_price DOUBLE not null,
-    sales_capacity double not null,
-    id_station int REFERENCES stations(id),
-    id_product int REFERENCES products(id)
-);
-
-CREATE TABLE IF NOT EXISTS supply_station(
-    id serial PRIMARY KEY,
-    supply_quantity DOUBLE not null,
-    supply_date timestamp not null DEFAULT current_timestamp,
-    id_station int REFERENCES stations(id),
-    id_product int REFERENCES products(id)
-);
 
 CREATE TABLE IF NOT EXISTS movement_stock(
     id serial PRIMARY KEY,
     movement_type VARCHAR(50) not null,
+    movement_quantity DOUBLE PRECISION  not null,
     movement_datetime timestamp not null default current_timestamp,
     id_station int REFERENCES stations(id),
     id_product int REFERENCES products(id)
 );
 
-CREATE TABLE IF NOT EXISTS station_and_product(
+CREATE TABLE IF NOT EXISTS stock(
+    id serial PRIMARY KEY,
+    product_capacity DOUBLE PRECISION  not null,
+    evaporation_rate int not null,
+    stock_datetime timestamp not null default current_timestamp,
     id_station int REFERENCES stations(id),
     id_product int REFERENCES products(id)
 );
